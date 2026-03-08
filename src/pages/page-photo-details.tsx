6edad29da7188efb5956064e7393f506f40cdd6c
RafaSMK <rafael012chavess@gmail.com>
2026-03-08 18:59:09 -0300

    09. Mock da página de detalhes da foto - Imagem e botão de excluir

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -3,10 +3,12 @@ import Container from "../components/container";
 import type { Photo } from "../contexts/photos/models/photo";
 import Skeleton from "../components/skeleton";
 import PhotosNavigator from "../contexts/photos/components/photos-navigator";
+import ImagePreview from "../components/image-preview";
+import Button from "../components/button";
 
 export default function PagePhotoDetails() {
   //Apenas para fazer o teste do mock
-  const isLoadingPhoto = false;
+  const isLoadingPhoto = true;
   const photo = {
     id: "123",
     title: "Olá mundo!",
@@ -28,6 +30,25 @@ export default function PagePhotoDetails() {
         )}
         <PhotosNavigator loading={isLoadingPhoto} />
       </header>
+
+      <div className="grid grid-cols-[21] gap-24">
+        <div className="space-y-3">
+          {!isLoadingPhoto ? (
+            <ImagePreview
+              src={`/images/${photo?.imageId}`}
+              title={photo.title}
+              imageClassName="h-[21rem]"
+            />
+          ) : (
+            <Skeleton className="h-[21rem]" />
+          )}
+          {!isLoadingPhoto ? (
+            <Button variant="destructive">Excluir</Button>
+          ) : (
+            <Skeleton className="w-20 h-10" />
+          )}
+        </div>
+      </div>
     </Container>
   );
 }
